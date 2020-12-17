5e4491d1c5706932778573dce5b4d813d879b1bf
silolmo <silvia.olmos.lara@gmail.com>
2020-12-17 11:25:19 +0100

    Moverse por el menu del ciudadano

--- app/src/main/java/com/example/urclean/MenuCiudadanoActivity.java
+++ app/src/main/java/com/example/urclean/MenuCiudadanoActivity.java
@@ -1,27 +1,57 @@
 package com.example.urclean;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.urclean.fragments.IncidenciaCiudadanoFragment;
+import com.example.urclean.fragments.NotificacionesCiudadanoFragment;
+import com.example.urclean.fragments.PerfilCiudadanoFragment;
+import com.example.urclean.fragments.QuejasFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MenuCiudadanoActivity extends AppCompatActivity {
-
-    private Button botonQuejas;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuciudadano);
-        botonQuejas = (Button)findViewById(R.id.botonQuejasCiu);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new PerfilCiudadanoFragment()).commit();
+        }
 
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_perfil:
+                            selectedFragment = new PerfilCiudadanoFragment();
+                            break;
+                        case R.id.nav_incidencias:
+                            selectedFragment = new IncidenciaCiudadanoFragment();
+                            break;
+                        case R.id.nav_quejas:
+                            selectedFragment = new QuejasFragment();
+                            break;
+                        case R.id.nav_notificaciones:
+                            selectedFragment = new NotificacionesCiudadanoFragment();
+                            break;
+                    }
 
-    public void clickQuejas(View v){
-        //Creamos el intent
-        Intent intent = new Intent(MenuCiudadanoActivity.this, QuejasActivity.class);
-        startActivity(intent);
-    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
