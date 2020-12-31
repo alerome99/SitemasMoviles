3b438213ce2a7eeb22ea99788d3e877952ee3ae0
alerome99 <a.romeropacho1999@gmail.com>
2020-12-31 02:28:16 +0100

    para que se pueda añadir una foto de perfil a un usuario y cuando este inicie sesion le aparezca

--- app/src/main/java/com/example/urclean/fragments/PerfilCiudadanoFragment.java
+++ app/src/main/java/com/example/urclean/fragments/PerfilCiudadanoFragment.java
@@ -1,18 +1,37 @@
 package com.example.urclean.fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.urclean.R;
+import com.example.urclean.firebase.FirebaseCallback;
 import com.example.urclean.firebase.FirebaseConnection;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import static android.app.Activity.RESULT_OK;
 
 public class PerfilCiudadanoFragment extends Fragment implements View.OnTouchListener{
 
@@ -22,6 +41,10 @@ public class PerfilCiudadanoFragment extends Fragment implements View.OnTouchLis
     TextView textViewUsername;
     TextView testViewPhone;
     TextView textViewDni;
+    ImageView imageViewPhoto;
+
+    private StorageReference mStorage;
+    private static final int GALLERY_INTENT = 1;
 
     @Nullable
     @Override
@@ -33,7 +56,16 @@ public class PerfilCiudadanoFragment extends Fragment implements View.OnTouchLis
         textViewUsername = view.findViewById(R.id.textViewUsername);
         testViewPhone = view.findViewById(R.id.textViewTelefono);
         textViewDni = view.findViewById(R.id.textViewDni);
+        imageViewPhoto = view.findViewById(R.id.imageViewPhoto);
+        mStorage = FirebaseStorage.getInstance().getReference();
 
+
+        imageViewPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarImagen();
+            }
+        });
         connection = FirebaseConnection.getInstance();
         connection.getPersona(correct -> {
             if (correct){
@@ -45,12 +77,18 @@ public class PerfilCiudadanoFragment extends Fragment implements View.OnTouchLis
                     String username = "";
                     String phone = "";
                     String dni = "";
+                    String url  = "";
                     for (QueryDocumentSnapshot document : connection.getResponse()){
                         email = (String) document.get("email");
                         name = (String) document.get("name");
                         username = (String) document.get("username");
                         phone = (String) document.get("telefono");
                         dni = (String) document.get("dni");
+                        url = (String) document.get("Foto");
+                    }
+                    if(url!=null){
+                        Uri path = Uri.parse(url);
+                        Glide.with(getActivity()).load(path).fitCenter().centerCrop().into(imageViewPhoto);
                     }
                     textViewEmail.setText(email);
                     textViewName.setText(name);
@@ -66,6 +104,71 @@ public class PerfilCiudadanoFragment extends Fragment implements View.OnTouchLis
         view.findViewById(R.id.botonEditar).setOnTouchListener(this);
 
         return view;
+    }
+
+    private void cargarImagen() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_INTENT);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == GALLERY_INTENT){
+            Uri path=data.getData();
+            StorageReference filePath = mStorage.child("fotos").child(path.getLastPathSegment());
+            final UploadTask uploadTask = filePath.putFile(path);
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+
+                            }
+                            return filePath.getDownloadUrl();
+
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()) {
+                                Glide.with(getActivity()).load(task.getResult().toString()).fitCenter().centerCrop().into(imageViewPhoto);
+                                String pathS = task.getResult().toString();
+                                connection.getUsuarioPorEmail(textViewEmail.getText().toString(), correct -> {
+                                    if (correct){
+                                        if (connection.getResponse().isEmpty() || connection.getResponse() == null){
+                                        }else{
+                                            String id = "";
+                                            for (QueryDocumentSnapshot document : connection.getResponse()) {
+                                                id = (String) document.getId();
+                                            }
+                                            connection.addFoto(id, pathS, new FirebaseCallback() {
+                                                @Override
+                                                public void onResponse(boolean correct) {
+                                                    if (correct) {
+                                                    } else {
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }else{
+                                    }
+                                });
+                            }
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                        }
+                    });
+                }
+            });
+        }
     }
 
     @Override
