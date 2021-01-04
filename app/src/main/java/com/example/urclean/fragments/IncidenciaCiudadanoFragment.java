bdf278e04086b7376f49d59ebb5c46a58025fdad
silolmo <silvia.olmos.lara@gmail.com>
2021-01-04 16:58:53 +0100

    gestión de errores en incidencias

--- app/src/main/java/com/example/urclean/fragments/IncidenciaCiudadanoFragment.java
+++ app/src/main/java/com/example/urclean/fragments/IncidenciaCiudadanoFragment.java
@@ -2,13 +2,13 @@ package com.example.urclean.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -22,7 +22,8 @@ import com.google.android.material.snackbar.Snackbar;
 public class IncidenciaCiudadanoFragment extends Fragment {
 
     private FirebaseConnection connection;
-    private EditText direccion, descripcion;
+    private EditText descripcion;
+    private TextView direccion;
     private Spinner spinner;
     private String lat,lng,dir,cod;
     @Nullable
@@ -39,7 +40,7 @@ public class IncidenciaCiudadanoFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
 
-        direccion = view.findViewById(R.id.editTextDireccionIncidencia);
+        direccion = view.findViewById(R.id.textViewDireccionIncidencia);
         descripcion = view.findViewById(R.id.editTextDescripcionIncidencia);
 
         if(getArguments()!=null){
@@ -54,37 +55,44 @@ public class IncidenciaCiudadanoFragment extends Fragment {
         view.findViewById(R.id.botonEnviarIncidencia).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String item = spinner.getSelectedItem().toString();
-                Log.d("onTouch", item);
-                switch (item){
-                    case "Limpieza":
-                        connection.saveTarea(dir, cod, descripcion.getText().toString(), new FirebaseCallback() {
-                            @Override
-                            public void onResponse(boolean correct) {
-                                if(correct){
-                                    direccion.setText("");
-                                    descripcion.setText("");
-                                    Snackbar.make(view, "Incidencia enviada", Snackbar.LENGTH_LONG).show();
-                                }else{
-                                    Snackbar.make(view, "Ha habido un error", Snackbar.LENGTH_LONG).show();
+                if(direccion.getText().toString().trim().length() == 0){
+                    direccion.setError("Ingrese el lugar de la incidencia");
+                }
+                if(descripcion.getText().toString().trim().length() == 0){
+                    descripcion.setError("Ingrese el motivo de la incidencia");
+                }
+                if(direccion.getText().toString().trim().length() != 0 && descripcion.getText().toString().trim().length() != 0){
+                    String item = spinner.getSelectedItem().toString();
+                    switch (item){
+                        case "Limpieza":
+                            connection.saveTarea(dir, cod, descripcion.getText().toString(), new FirebaseCallback() {
+                                @Override
+                                public void onResponse(boolean correct) {
+                                    if(correct){
+                                        direccion.setText("");
+                                        descripcion.setText("");
+                                        Snackbar.make(view, "Incidencia enviada", Snackbar.LENGTH_LONG).show();
+                                    }else{
+                                        Snackbar.make(view, "Ha habido un error", Snackbar.LENGTH_LONG).show();
+                                    }
                                 }
-                            }
-                        });
-                        break;
-                    case "Desperfecto":
-                        connection.saveDesperfecto(dir, lat, lng, descripcion.getText().toString(), new FirebaseCallback() {
-                            @Override
-                            public void onResponse(boolean correct) {
-                                if(correct){
-                                    direccion.setText("");
-                                    descripcion.setText("");
-                                    Snackbar.make(view, "Desperfecto enviado", Snackbar.LENGTH_LONG).show();
-                                }else{
-                                    Snackbar.make(view, "Ha habido un error", Snackbar.LENGTH_LONG).show();
+                            });
+                            break;
+                        case "Desperfecto":
+                            connection.saveDesperfecto(dir, lat, lng, descripcion.getText().toString(), new FirebaseCallback() {
+                                @Override
+                                public void onResponse(boolean correct) {
+                                    if(correct){
+                                        direccion.setText("");
+                                        descripcion.setText("");
+                                        Snackbar.make(view, "Desperfecto enviado", Snackbar.LENGTH_LONG).show();
+                                    }else{
+                                        Snackbar.make(view, "Ha habido un error", Snackbar.LENGTH_LONG).show();
+                                    }
                                 }
-                            }
-                        });
-                        break;
+                            });
+                            break;
+                    }
                 }
             }
         });
